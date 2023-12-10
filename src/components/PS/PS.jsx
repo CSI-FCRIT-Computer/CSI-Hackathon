1dbe5bbf37215669656085791090f13ffeb311c5
Sojith Sunny <72255758+sojith29034@users.noreply.github.com>
2023-12-10 16:52:30 +0000

    ps update

--- src/components/PS/PS.jsx
+++ src/components/PS/PS.jsx
@@ -2,52 +2,11 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion as m } from "framer-motion";
 import "./PS.css";
+import ps from "./ps.json"
 import img1 from "./proj/thumb1.jpg";
 
-const cardsData = [
-  {
-    id: 1,
-    imageUrl: img1,
-    text: "Card 1",
-    type: "Website",
-    link: "https://example.com/card1"
-  },
-  {
-    id: 2,
-    imageUrl: "path-to-image-2",
-    text: "Card 2",
-    type: "Website",
-    link: "https://example.com/card2"
-  },
-  {
-    id: 3,
-    imageUrl: "path-to-image-2",
-    text: "Card 2",
-    type: "Website",
-    link: "https://example.com/card2"
-  },
-  {
-    id: 4,
-    imageUrl: "path-to-image-2",
-    text: "Card 2",
-    type: "Design",
-    link: "https://example.com/card2"
-  },
-  {
-    id: 5,
-    imageUrl: "path-to-image-2",
-    text: "Card 2",
-    type: "Design",
-    link: "https://example.com/card2"
-  },
-  {
-    id: 6,
-    imageUrl: "path-to-image-2",
-    text: "Card 2",
-    type: "Design",
-    link: "https://example.com/card2"
-  }
-];
+const web = ps.filter(card => card.type === "web");
+const design = ps.filter(card => card.type === "design");
 
 const Projects = () => {
   const [closed, setClosed] = useState(false);
@@ -59,6 +18,7 @@ const Projects = () => {
       navigate("/");
     }, 900);
   };
+
   return (
     <div className="container">
       <m.div
@@ -66,38 +26,65 @@ const Projects = () => {
         animate={{ x: closed ? "-100%" : "0%" }}
         exit={{ x: "-100%" }}
         transition={{ duration: 0.75, ease: "easeOut" }}
-        style={{ background: "#eee", height: "100vh" }}
+        style={{ background: "transparent", height: "100vh" }}
         className="background"
       >
 
+        <h1 className="title">Problem Statements</h1>
+        
+        <div className="card-container">
 
-        <div className="content1">
-          <div className="proj-info">
-            <h3>Problem Statements</h3>
-            <p>
-              Some of my recent work is showcased below, allowing you to explore
-              the projects and the techstack I used for each one.
-            </p>
+          <div className="container">
+            <div className="card">
+              <h1 className="section-title">Web & App</h1>
+              
+              <div className="section">   
+                {web.map((card)=>(         
+                  <div className="statement">
+                    {card.ps}
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="card-container">
-            {cardsData.map((card) => (
-              <a key={card.id} href={card.link} className="card">
-                <div
-                  className="card-image"
-                  style={{ backgroundImage: `url(${card.imageUrl})` }}
-                ></div>
-                <div className="card-text">
-                  {card.text}
-                  <p>{card.type}</p>
-                </div>
-              </a>
-            ))}
+          
+          <div className="container">
+            <div className="card">
+              <h1 className="section-title">Design</h1>
+              
+              <div className="section">   
+                {design.map((card)=>(         
+                  <div className="statement">
+                    {card.ps}
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="close-div">
-            <button className="close" onClick={handleButtonClick}>
-              close
-            </button>
+          
+          <div className="container">
+            <div className="card">
+              <h1 className="section-title">Design65432</h1>
+              
+              <div className="section">   
+                {design.map((card)=>(         
+                  <div className="statement">
+                    {card.ps}
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
+
+        </div>
+        
+
+
+        
+        <div className="close-div">
+          <button className="close" onClick={handleButtonClick}>
+            close
+          </button>
         </div>
       </m.div>
     </div>
