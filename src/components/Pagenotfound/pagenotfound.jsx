9e12fa03eb8dfe60145aed120ee3e48b65c425c2
Savio Dias <91362589+Savio629@users.noreply.github.com>
2023-12-17 16:07:06 +0000

    added 404, buttons and waves

--- src/components/Pagenotfound/pagenotfound.jsx
+++ src/components/Pagenotfound/pagenotfound.jsx
@@ -3,10 +3,10 @@ import './pagenotfound.css';
 
 const pagenotfound = () => {
   return (
-    <div className="container">
-      <h1>:(</h1><br />
-      <h2>A <span>404</span> error occurred, Page not found, check the URL and try again.</h2><br /><br />
-      <h3><a href="#">Return to home</a>&nbsp;|&nbsp;<a href="javascript:history.back()">Go Back</a></h3>
+    <div className="container404">
+      <div className="face404">:(</div><br />
+      <div className="text404">A <span>404</span> error occurred, Page not found, check the URL and try again.</div><br /><br />
+      <div className="subtext404"><a href="/">Return to home</a>&nbsp;|&nbsp;<a href="javascript:history.back()">Go Back</a></div>
     </div>
   );
 };
